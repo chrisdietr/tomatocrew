379257962e6554e3131225c15bc4c42ce2f4c7d0
Dustin Laverick <dustin@laverick.org>
2015-01-24 16:24:49 +0700

    Add task_start, forward to working

---
+++ client/templates/task_start.js
@@ -0,0 +1,36 @@
+Template.taskStart.events({ 
+  'submit form': function(e) {
+    // e.preventDefault();
+
+    console.log("Form submitted");
+
+
+    // var task = {
+    //   name : $(e.target).find('[name=url]').val(), 
+    //   duration: $(e.target).find('[name=duration]').val(), 
+    // }
+
+    // var post = {
+    //   url: $(e.target).find('[name=url]').val(), 
+    //   title: $(e.target).find('[name=title]').val()
+    // };
+
+    // Meteor.call('postInsert', post, function(error, result) { 
+    //   // display the error to the user and abort
+    //   if (error) {
+    //     return alert(error.reason);        
+    //   }
+
+    //   // show this result but route anyway
+    //   if (result.postExists) {
+    //     alert('This link has already been posted');
+    //   }
+
+    //   Router.go('postPage', {_id: result._id});
+    // });
+
+    Router.go('/working');
+
+    return false;
+  }
+});
