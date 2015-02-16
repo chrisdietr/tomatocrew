5eb460118b30982eb3d5ae3807bcf7643090447f
Dustin Laverick <dustin@laverick.org>
2015-02-16 15:02:35 +0800

    Only show online users

--- client/templates/working.js
+++ client/templates/working.js
@@ -1,9 +1,12 @@
+Meteor.subscribe("userStatus");
+
 Template.working.helpers ({
-  usersOnline : function() { return Meteor.users.find().fetch() }
-  // usersOnline : function() { return Meteor.users.find({ "status.online": true }).fetch() }
+  // usersOnline : function() { return Meteor.users.find().fetch() }
+  usersOnline : function() { return Meteor.users.find({ "status.online": true }).fetch(); }
 });
 
 Template.userPill.helpers ({
   labelClass : function() { return this.username }
 
 });
+
