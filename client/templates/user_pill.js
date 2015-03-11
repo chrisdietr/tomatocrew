4a29cb12e8c2976308b93c78b4291dcbf36808eb
Dustin Laverick <dustin@laverick.org>
2015-03-11 20:52:11 +0700

    Show activities. Show user online state.

--- client/templates/user_pill.js
+++ client/templates/user_pill.js
@@ -1,14 +1,12 @@
-// NOTE: status is currently missing from the client publication of the user
-
-// Template.userPill.labelClass = function() {
-//   if (this.status.idle)
-//     return "label-warning"
-//   else if (this.status.online)
-//     return "label-success"
-//   else
-//     return "label-default"
-// };
-
-// Template.userPill.helpers ({
-//   labelClass : function() { return this.username }
-// });
+Template.userPill.helpers ({
+  userStatusClass : function() { 
+    // if (this.user.status.idle)
+    //  return "user-idle"
+    // else
+    
+    if (this.user.status.online) 
+      return "user-online"
+    else 
+      return "user-offline"
+  }
+});
--- client/templates/working.js
+++ client/templates/working.js
@@ -1,29 +1,40 @@
 Meteor.subscribe("userStatus");
 
 Template.working.helpers ({
-  // usersOnline : function() { return Meteor.users.find().fetch() }
-  usersOnline : function() { 
-    var users = Meteor.users.find({ "status.online": true }).fetch(); 
-    // Tasks.find()
-    // console.log("users " + users);
-
+  latestUsers : function() { 
+    var users = Meteor.users.find().fetch(); 
     var userTaskMap = {};
     userTaskMap = users.map(
       function(user){
-        // console.log("user " + user);
-        // console.log("userId " + user._id);
         topTask = Tasks.findOne({userId: user._id}, {sort: {submitted: -1}});
-        // console.log("topTask.name " + topTask.name);
         return { 'user':user, 'task':topTask}
       }
     );
-
-    // console.log("userTaskMap ");
-    // console.log(userTaskMap);
-    // userTaskMap.map ( function(doc) { console.log(doc)});
-    // Tasks.findOne({userId: 'yTDMbtm5zBmADGTiX'}, {sort: {submitted: -1}});
-
-    // return users;
     return userTaskMap;
+  },
+
+  // usersOnline : function() { 
+  //   var users = Meteor.users.find({ "status.online": true }).fetch(); 
+  //   var userTaskMap = {};
+  //   userTaskMap = users.map(
+  //     function(user){
+  //       topTask = Tasks.findOne({userId: user._id}, {sort: {submitted: -1}});
+  //       return { 'user':user, 'task':topTask}
+  //     }
+  //   );
+  //   return userTaskMap;
+  // },
+
+  latestTasks : function() { 
+    var tasks = Tasks.find({},{sort: {submitted: -1}}).fetch();
+    console.log("tasks " + tasks);
+
+    // DFL TODO:
+    // get unique user IDs from tasks
+    // find username for each 
+    // build retval with username + task
+
+    return tasks;
   }
+
 });
