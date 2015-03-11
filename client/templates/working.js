b5c60f4b56f4ab17cf011c7f0f4d753c212c7b00
Dustin Laverick <dustin@laverick.org>
2015-03-11 22:40:46 +0700

    Add users to activity stream. NOTE - very inefficient lookup

--- client/templates/working.js
+++ client/templates/working.js
@@ -2,7 +2,8 @@ Meteor.subscribe("userStatus");
 
 Template.working.helpers ({
   latestUsers : function() { 
-    var users = Meteor.users.find().fetch(); 
+    var users = Meteor.users.find({},{limit:50}).fetch(); 
+    //   var users = Meteor.users.find({ "status.online": true }).fetch(); 
     var userTaskMap = {};
     userTaskMap = users.map(
       function(user){
@@ -13,28 +14,19 @@ Template.working.helpers ({
     return userTaskMap;
   },
 
-  // usersOnline : function() { 
-  //   var users = Meteor.users.find({ "status.online": true }).fetch(); 
-  //   var userTaskMap = {};
-  //   userTaskMap = users.map(
-  //     function(user){
-  //       topTask = Tasks.findOne({userId: user._id}, {sort: {submitted: -1}});
-  //       return { 'user':user, 'task':topTask}
-  //     }
-  //   );
-  //   return userTaskMap;
-  // },
-
   latestTasks : function() { 
-    var tasks = Tasks.find({},{sort: {submitted: -1}}).fetch();
+    var tasks = Tasks.find({},{sort: {submitted: -1}, limit:50}).fetch();
     console.log("tasks " + tasks);
-
-    // DFL TODO:
-    // get unique user IDs from tasks
-    // find username for each 
-    // build retval with username + task
-
-    return tasks;
+    var tasksUserMap = tasks.map(
+      function(task){
+        // DFL TODO: This wastes a bunch of work as we repeatedly fetch the same user
+        var user = Meteor.users.findOne({_id : task.userId});
+        // console.log("user ._id .username" + user + ' ' + user._id + ' ' + user.username);
+        return _.extend(task,{username:user.username});
+      }
+    );
+    return tasksUserMap;
   }
+
 
 });
