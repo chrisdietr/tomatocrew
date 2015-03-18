b6c6bc245e10016ec79e28241751a13f269cec21
Dustin Laverick <dustin@laverick.org>
2015-03-18 17:27:52 +0700

    Refactor -  take timer_modal functions out of task_form.js

--- client/templates/task_form.js
+++ client/templates/task_form.js
@@ -2,7 +2,6 @@
 Template.taskForm.events({ 
   'submit form': function(e) {
     e.preventDefault();
-
     console.log("Form submitted");
 
     var task = {
@@ -16,128 +15,9 @@ Template.taskForm.events({
         return alert(error.reason);        
       }
 
-      setModalTitleText("Working. Focus!");
-      setModalTimerTask(task.name);
-
-      var onComplete = function(){
-        setModalTitleText("Aaah, time to relax.");
-
-        setTaskEditability(true);
-
-        runModalTimer(3, function(){
-          setModalTitleText("DONE")
-        });
-      };
-
-      runModalTimer(task.intervalDuration, onComplete);
-
-      // Router.go('/working');
+      // DFL TODO: Bind the timer setup to a reactive Variable instead of invoking directly
+      dummyTimerSetup(task.intervalDuration);
     });
-    // Router.go('/working');
-    // return false;
   }
 });
 
-
-// DFL TODO: Extract timer to separate class, handle html manipulation here
-
-var setTaskEditability = function(editable) {
-  if (editable) {
-    document.getElementById("timerModalTaskField").removeAttribute('hidden');
-    document.getElementById("timerModalTask").setAttribute('hidden', '');
-  } else {
-    document.getElementById("timerModalTaskField").setAttribute('hidden');
-    document.getElementById("timerModalTask").removeAttribute('hidden');    
-  }
-}
-
-var setModalTitleText = function(text) {
-  document.getElementById("timerModalTitle").innerHTML = text;
-}
-
-var setModalTimerText = function(text) {
-  document.getElementById("timerModalTime").innerHTML = text;
-}
-
-var setModalTimerTask = function(text) {
-  document.getElementById("timerModalTask").innerHTML = text;
-  document.getElementById("taskField").value = text;
-}
-
-var showTimerModalTaskField = function(text) {
-  document.getElementById("timerModalTaskField").innerHTML = text;
-}
-
-var pad2 = function(n) {
-  n = n + '';
-  return n.length > 1 ? n : '0' + n;
-}
-
-var clockFormat = function(date) {
-  return pad2(date.getUTCHours()) + ":" + pad2(date.getUTCMinutes()) + ":" + pad2(date.getUTCSeconds());
-}
-
-
-// DFL TODO: Scope & lifetime conflicts 
-
-var runModalTimer = function(timeleft, onComplete) {
-  var counter = setInterval(timer, 1000); // milliseconds
-  var startDate = new Date();
-  var finishDate = startDate.getHours(startDate.getHours()+timeleft);
-  
-  setModalTimerText('');
-  $('#timerModal').modal();
-
-  setModalTimerText(clockFormat(new Date(finishDate - Date.now())));
-
-  function timer() {
-    --timeleft;
-    if (timeleft <= 1) {
-      clearInterval(counter);
-      //counter ended, do something here
-      setModalTimerText('');
-      onComplete();
-      return;
-    }
-
-    setModalTimerText( 
-      clockFormat( new Date(finishDate - Date.now()) ) 
-    );
-  }
-};
-
-
-
-// var runModalTimer = (function(){
-
-//   var counter;
-//   var startDate;
-//   var finishDate;
-//   setModalTimerText('');
-
-
-//   return function() {
-//     $('#timerModal').modal();
-//     var counter = setInterval(timer, 1000); // milliseconds
-//     var startDate = new Date();
-//     var finishDate = startDate.setSeconds(startDate.getSeconds()+timeleft);
-//     setModalTimerText(clockFormat(new Date(finishDate - Date.now())));
-
-//     function timer() {
-//       --timeleft;
-//       if (timeleft <= 1) {
-//       clearInterval(counter);
-//       //counter ended, do something here
-//       setModalTimerText('');
-//       onComplete();
-//       return;
-//     }
-
-//     setModalTimerText( 
-//       clockFormat( new Date(finishDate - Date.now()) ) 
-//     );
-    
-//     }
-
-//   };
-// })();
---
+++ client/templates/timer_modal.js
@@ -0,0 +1,72 @@
+
+dummyTimerSetup = function (duration) {
+      setModalTitleText("Working. Focus!");
+      setModalTimerTask(task.name);
+      runModalTimer(task.intervalDuration, onDummyTimerComplete);
+};
+
+var onDummyTimerComplete = function(){
+  setModalTitleText("Aaah, time to relax.");
+  setTaskEditability(true);
+  runModalTimer(3, function(){
+    setModalTitleText("DONE")
+  });
+};
+
+Template.timerModal.prototype.
+
+var setTaskEditability = function(editable) {
+  if (editable) {
+    document.getElementById("timerModalTaskField").removeAttribute('hidden');
+    document.getElementById("timerModalTask").setAttribute('hidden', '');
+  } else {
+    document.getElementById("timerModalTaskField").setAttribute('hidden');
+    document.getElementById("timerModalTask").removeAttribute('hidden');    
+  }
+}
+
+var setModalTitleText = function(text) {
+  document.getElementById("timerModalTitle").innerHTML = text;
+}
+
+var setModalTimerText = function(text) {
+  document.getElementById("timerModalTime").innerHTML = text;
+}
+
+var setModalTimerTask = function(text) {
+  document.getElementById("timerModalTask").innerHTML = text;
+  document.getElementById("taskField").value = text;
+}
+
+var showTimerModalTaskField = function(text) {
+  document.getElementById("timerModalTaskField").innerHTML = text;
+}
+
+
+// DFL TODO: Scope & lifetime conflicts 
+
+var runModalTimer = function(timeleft, onComplete) {
+  var counter = setInterval(timer, 1000); // milliseconds
+  var startDate = new Date();
+  var finishDate = startDate.getHours(startDate.getHours()+timeleft);
+  
+  setModalTimerText('');
+  $('#timerModal').modal();
+
+  setModalTimerText((new Date(finishDate - Date.now()).clockFormat()));
+
+  function timer() {
+    --timeleft;
+    if (timeleft <= 1) {
+      clearInterval(counter);
+      //counter ended, do something here
+      setModalTimerText('');
+      onComplete();
+      return;
+    }
+
+    setModalTimerText( 
+      (new Date(finishDate - Date.now())).clockFormat()
+    );
+  }
+};
