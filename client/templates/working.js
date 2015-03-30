61cc203d57bd7917d9f36816e6bb96c83dcca670
Dustin Laverick <dustin@laverick.org>
2015-03-30 14:24:16 +0800

    Make variables local. Reformat file.

--- client/templates/working.js
+++ client/templates/working.js
@@ -1,39 +1,35 @@
 Meteor.subscribe("activeUsers");
 Meteor.subscribe("tasksList");
 
-Template.working.helpers ({
+Template.working.helpers({
 
-  latestTasks : function() { 
-    var tasks = Tasks.find({},{sort: {submitted: -1}, limit:50}),
-        tasksIds = _.pluck(tasks.fetch(), '_id'),
-        userIds = _.unique(_.pluck(tasks.fetch(), 'userId')),
-        userObjects = Meteor.users.find({_id: {$in: userIds}}, {fields: {username: 1, _id: 1}, multi: true}).fetch(),
-        userIdsNamesDict = {};
+  latestTasks: function() {
+    var tasks = Tasks.find({}, {sort: {submitted: -1}, limit: 50});
+    var userIds = _.unique(_.pluck(tasks.fetch(), 'userId'));
+    var userObjects = Meteor.users.find({_id: {$in: userIds}}, {fields: {username: 1, _id: 1}, multi: true}).fetch();
+    var userIdsNamesDict = {};
+    var tasksUserMap;
 
-        userObjects.map( function(userObj) {
-          userIdsNamesDict[userObj._id] = userObj.username;
-        });
-        // console.log(userIdsNamesDict);
+    userObjects.map(function(userObj) {
+      userIdsNamesDict[userObj._id] = userObj.username;
+    });
 
-        tasksUserMap = tasks.map( function(task) {
-          return _.extend(task, {username: userIdsNamesDict[task.userId]});
-        });
-        // console.log(tasksUserMap);
+    tasksUserMap = tasks.map(function(task) {
+      return _.extend(task, {username: userIdsNamesDict[task.userId]});
+    });
 
-        return tasksUserMap;
+    return tasksUserMap;
   },
 
-  latestUsers : function() { 
-    var users = Meteor.users.find({},{limit:50}).fetch(); 
+  latestUsers: function() {
+    var users = Meteor.users.find({}, {limit: 50}).fetch();
     //   var users = Meteor.users.find({ "status.online": true }).fetch(); 
-    var userTaskMap = {};
-    userTaskMap = users.map(
-      function(user){
-        topTask = Tasks.findOne({userId: user._id}, {sort: {submitted: -1}});
-        return { 'user':user, 'task':topTask}
+    var userTaskMap = users.map(
+      function(user) {
+        var topTask = Tasks.findOne({userId: user._id}, {sort: {submitted: -1}});
+        return {'user': user, 'task': topTask}
       }
     );
     return userTaskMap;
-  },
-
+  }
 });
